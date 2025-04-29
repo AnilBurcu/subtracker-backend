245e74ec0402611f7a990562592ce754541e8a5e
AnilBurcu <anlbrc95@gmail.com>
2025-04-29 13:50:43 +0300

    first commit

---
+++ routes/user.routes.js
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+
+const userRouter = Router();
+//GET /users -> get all users
+//GET /users/:id -> get users by id
+
+userRouter.get('/',(req,res)=>res.send({title:'GET all users'}))
+userRouter.get('/:id',(req,res)=>res.send({title:'GET user details'}))
+userRouter.post('/',(req,res)=>res.send({title:'CREATE new user'}))
+userRouter.put('/:id',(req,res)=>res.send({title:'UPTADE user'}))
+userRouter.delete('/:id',(req,res)=>res.send({title:'DELETE user'}))
+
+export default userRouter;
