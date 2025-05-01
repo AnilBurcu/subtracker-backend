e836b328dc2cb9e555ccfa8ae7c8172c69106dc6
AnilBurcu <anlbrc95@gmail.com>
2025-05-01 12:38:59 +0300

    Spam Protection with Arcjet / Subscription fix

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ import authRouter from "./routes/auth.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import connectToDatabase from "./database/mongodb.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
+import arcjetMiddleware from "./middlewares/arcjet.middleware.js";
 
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser())
+app.use(arcjetMiddleware)
 
 // api/v1/auth/sign-up
 app.use('/api/v1/auth',authRouter)
